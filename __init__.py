4f4652fccfdd8640850f60181d5f9a7e707688ad
David Kamer <david.kamer@uniwrighte.com>
2019-01-31 13:59:09 -0500

    fixed py_modules, added .gitignore

--- __init__.py
+++
@@ -1,3 +0,0 @@
-__all__ = ['bashify']
-
-name = "bashify"
--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="bashify",
-	version="0.1.1",
+	version="0.1.3",
 	author="David Kamer",
 	author_email="me@davidkamer.com",
 	description="A package to print and execture bash files",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	url="https://github.com/UniWrighte/bashify",
-	packages=setuptools.find_packages(),
+	py_modules=["bashify"],
 	classifiers=[
 		"Programming Language :: Python :: 3",
 		"License :: OSI Approved :: MIT License",
